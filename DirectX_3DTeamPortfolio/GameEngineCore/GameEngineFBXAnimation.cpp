0262676b56ecb34c5d1971ae4219bd79363c460e
chang-ha <kym637@naver.com>
2023-12-22 01:34:18 +0900

    우창하 : 업데이트

--- DirectX_3DTeamPortfolio/GameEngineCore/GameEngineFBXAnimation.cpp
+++ DirectX_3DTeamPortfolio/GameEngineCore/GameEngineFBXAnimation.cpp
@@ -250,15 +250,15 @@ bool GameEngineFBXAnimation::AnimationLoad(std::shared_ptr <GameEngineFBXMesh> _
 			linkName = pCurrCluster->GetLink()->GetName();
 			fbxsdk::FbxNode* pLinkNode = Scene->FindNodeByName(linkName.c_str());
 
-			if (nullptr == pLinkNode)
-			{
-				continue;
-			}
-
 			FbxExBoneFrame& Frame = CurAniData.AniFrameData[pBone->Index];
 			Frame.BoneMatData.resize(endTime - startTime + 1);
 			Frame.BoneIndex = pBone->Index;
 			Frame.BoneParentIndex = pBone->ParentIndex;
+
+			if (nullptr == pLinkNode)
+			{
+				continue;
+			}
 
 			for (fbxsdk::FbxLongLong i = startTime; i <= endTime; ++i)
 			{
