0a8b970d664dd1585928d380e655a3412f60697a
김태훈 <hyri0510@gmail.com>
2023-12-22 17:03:16 +0900

    김태훈 : [Style] Filter 추가

--- DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
@@ -13,8 +13,13 @@ Monster_LothricKn::~Monster_LothricKn()
 void Monster_LothricKn::Start()
 {
 	BodyRenderer = CreateComponent<GameEngineFBXRenderer>();
-	BodyRenderer->SetFBXMesh("c1240.fbx", "FBXColor");
+	BodyRenderer->SetFBXMesh("c1280.fbx", "FBXColor");
 	BodyRenderer->Transform.SetLocalScale(float4(50.0f, 50.0f, 50.0f));
+	BodyRenderer->Transform.SetLocalRotation(float4(0.0f, 0.0f, -90.0f));
+
+	BodyRenderer->CreateFBXAnimation("Idle", "Ani002.fbx", {0.1f, true}, 0);
+	BodyRenderer->ChangeAnimation("Idle");
+
 }
 
 void Monster_LothricKn::Release()
