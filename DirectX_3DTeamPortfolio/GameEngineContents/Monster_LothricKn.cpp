cc246b91efc17196db1650f1bcb818f640705826
김태훈 <hyri0510@gmail.com>
2023-12-23 14:46:26 +0900

    김태훈 : [Update] c1280 로스릭 기사 텍스처 업로드

--- DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
@@ -4,14 +4,6 @@
 
 Monster_LothricKn::Monster_LothricKn() 
 {
-	GameEngineDirectory Dir;
-	Dir.MoveParentToExistsChild("ContentsResources");
-	Dir.MoveChild("ContentsResources\\Mesh\\Monster\\HighWall_LothricKnight");
-	std::vector<GameEngineFile> Files = Dir.GetAllFile({ ".fbx" });
-	for (GameEngineFile& pFile: Files)
-	{
-		GameEngineFBXMesh::Load(pFile.GetStringPath());
-	}
 }
 
 Monster_LothricKn::~Monster_LothricKn() 
@@ -21,8 +13,21 @@ Monster_LothricKn::~Monster_LothricKn()
 
 void Monster_LothricKn::Start()
 {
+	if (nullptr == GameEngineFBXMesh::Find("c1280.fbx"))
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParentToExistsChild("ContentsResources");
+		Dir.MoveChild("ContentsResources\\Mesh\\Monster\\HighWall_LothricKnight");
+		std::vector<GameEngineFile> Files = Dir.GetAllFile({ ".fbx" });
+		for (GameEngineFile& pFile : Files)
+		{
+			GameEngineFBXMesh::Load(pFile.GetStringPath());
+		}
+	}
+
+
 	BodyRenderer = CreateComponent<GameEngineFBXRenderer>();
-	BodyRenderer->SetFBXMesh("c1280.fbx", "FBXColor");
+	BodyRenderer->SetFBXMesh("c1280.fbx", "FBXTexture");
 	BodyRenderer->Transform.SetLocalScale(float4(50.0f, 50.0f, 50.0f));
 	BodyRenderer->Transform.SetLocalRotation(float4(0.0f, 0.0f, -90.0f));
 
