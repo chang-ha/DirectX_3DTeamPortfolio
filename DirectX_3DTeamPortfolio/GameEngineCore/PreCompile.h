a400868469185180bac8f27e070536fcec4c81c7
chang-ha <kym637@naver.com>
2023-12-28 01:49:58 +0900

    우창하 : PxConfig.h 추가

--- DirectX_3DTeamPortfolio/GameEngineCore/PreCompile.h
+++ DirectX_3DTeamPortfolio/GameEngineCore/PreCompile.h
@@ -21,3 +21,15 @@
 
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui.h"
+
+// PhysX
+#include "PxConfig.h"
+
+#pragma comment (lib, "PhysX_static_64.lib")
+#pragma comment (lib, "PhysXCharacterKinematic_static_64.lib")
+#pragma comment (lib, "PhysXCommon_static_64.lib")
+#pragma comment (lib, "PhysXCooking_static_64.lib")
+#pragma comment (lib, "PhysXExtensions_static_64.lib")
+#pragma comment (lib, "PhysXFoundation_static_64.lib")
+#pragma comment (lib, "PhysXPvdSDK_static_64.lib")
+#pragma comment (lib, "PhysXVehicle_static_64.lib")
