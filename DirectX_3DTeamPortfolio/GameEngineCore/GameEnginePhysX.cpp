f9d9222e9a71556bb189a4c39237019ae9aad621
chang-ha <kym637@naver.com>
2023-12-28 15:06:37 +0900

    우창하 : ErrorCallBack 구현 및 Cooking, Physic 생성 추가

--- DirectX_3DTeamPortfolio/GameEngineCore/GameEnginePhysX.cpp
+++ DirectX_3DTeamPortfolio/GameEngineCore/GameEnginePhysX.cpp
@@ -4,6 +4,9 @@
 PhysXErrorCallback  GameEnginePhysX::gErrorCallback = {};
 physx::PxDefaultAllocator  GameEnginePhysX::gDefaultAllocatorCallback = {};
 physx::PxFoundation* GameEnginePhysX::mFoundation = nullptr;
+physx::PxPvd* GameEnginePhysX::mPvd = nullptr;
+physx::PxPhysics* GameEnginePhysX::mPhysics = nullptr;
+physx::PxCooking* GameEnginePhysX::mCooking = nullptr;
 
 GameEnginePhysX::GameEnginePhysX()
 {
@@ -18,9 +21,68 @@ GameEnginePhysX::~GameEnginePhysX()
 void GameEnginePhysX::PhysXInit()
 {
 	mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gErrorCallback);
+	
+	if (nullptr == mFoundation)
+	{
+		MsgBoxAssert("Foundation 생성에 실패했습니다.");
+	}
+
+	bool  RecordMemoryAllocations = true; // 메모리 프로파일 On
+
+#ifdef _DEBUG
+	//mPvd = PxCreatePvd(*mFoundation);
+
+	//if (nullptr == mPvd)
+	//{
+	//	MsgBoxAssert("Pvd 생성에 실패했습니다.");
+	//}
+
+	// Default listening port is 5425 
+	// physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	//mPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+#endif
+
+	physx::PxTolerancesScale Scale = physx::PxTolerancesScale(); // Default Length = 1cm, Speed = 10(1m/s)
+
+	mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, Scale, RecordMemoryAllocations, mPvd);
+
+	if (nullptr == mPhysics)
+	{
+		MsgBoxAssert("Physics 생성에 실패했습니다.");
+	}
+
+	mCooking = PxCreateCooking(PX_PHYSICS_VERSION, *mFoundation, physx::PxCookingParams(Scale));
+
+	if (nullptr == mCooking)
+	{
+		MsgBoxAssert("Cooking 생성에 실패했습니다.");
+	}
 }
 
 void GameEnginePhysX::PhysXRelease()
 {
-	mFoundation->release();
+	if (nullptr != mCooking)
+	{
+		mCooking->release();
+		mCooking = nullptr;
+	}
+
+	if (nullptr != mPhysics)
+	{
+		mPhysics->release();
+		mPhysics = nullptr;
+	}
+
+	if (nullptr != mPvd)
+	{
+		mPvd->release();
+		mPvd = nullptr;
+	}
+
+	if (nullptr != mFoundation)
+	{
+		mFoundation->release();
+		mFoundation = nullptr;
+	}
 }
