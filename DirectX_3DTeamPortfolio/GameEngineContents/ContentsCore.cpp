ef886cbd7a0421a8bcf04c11744d0d85e28b33c4
KimJunYoung <kimchk1104@naver.com>
2024-01-02 02:42:53 +0900

    김준영 : 맵분할 병합

--- DirectX_3DTeamPortfolio/GameEngineContents/ContentsCore.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/ContentsCore.cpp
@@ -38,7 +38,7 @@ void ContentsCore::Start()
 	GameEngineCore::CreateLevel<TestLevel_Boss>("TestLevel_Boss");
 	GameEngineCore::CreateLevel<TestLevel_Monster>("TestLevel_Monster");
 	GameEngineCore::CreateLevel<TestLevel_Shader>("TestLevel_Shader");
-	//GameEngineCore::CreateLevel<TestLevel_Map>("TestLevel_Map");
+	GameEngineCore::CreateLevel<TestLevel_Map>("TestLevel_Map");
 
 	GameEngineCore::ChangeLevel("PlayLevel");
 }
