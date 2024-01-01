5480631d9cabd67ee3972b4b2dd437e06aa1e74d
chang-ha <kym637@naver.com>
2024-01-02 02:22:25 +0900

    우창하 : 원, 상자, 캡슐 형태 디버깅 테스트

--- DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_PhysX.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_PhysX.cpp
@@ -35,8 +35,42 @@ void TestLevel_PhysX::Start()
 
 	// create simulation
 	physx::PxMaterial* mMaterial = Physics->createMaterial(0.5f, 0.5f, 0.6f);
+	// Ground
 	physx::PxRigidStatic* groundPlane = PxCreatePlane(*Physics, physx::PxPlane(0, 1, 0, 50), *mMaterial);
 	Scene->addActor(*groundPlane);
+
+	// Box
+	float halfExtent = 10.5f;
+	physx::PxShape* Boxshape = Physics->createShape(physx::PxBoxGeometry(halfExtent, halfExtent, halfExtent), *mMaterial);
+	physx::PxShape* Sphereshape = Physics->createShape(physx::PxSphereGeometry(10), *mMaterial);
+	physx::PxShape* CapsuleShape = Physics->createShape(physx::PxCapsuleGeometry(5, 20), *mMaterial); // 캡슐이 똑바로 서있는 모양은 1/4Pi 만큼 회전 필요
+	physx::PxTransform t(physx::PxVec3(0));
+	physx::PxQuat Quat = physx::PxQuat(physx::PxHalfPi, physx::PxVec3(0, 0, 1));
+	physx::PxTransform localTm(physx::PxVec3(0.0f, 3.0f, -100.0f), Quat);
+	physx::PxRigidDynamic* DynamicActor = Physics->createRigidDynamic(localTm);
+	DynamicActor->attachShape(*CapsuleShape);
+	physx::PxRigidBodyExt::updateMassAndInertia(*DynamicActor, 10.0f);
+	Scene->addActor(*DynamicActor);
+
+	//physx::PxRigidStatic* StaticActor = Physics->createRigidStatic(localTm);
+	//StaticActor->attachShape(*CapsuleShape);
+	//Scene->addActor(*StaticActor);
+
+	// physx::PxU32 size = 30;
+	//for (physx::PxU32 i = 0; i < size; i++) {
+	//	for (physx::PxU32 j = 0; j < size - i; j++) {
+	//		physx::PxTransform localTm(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 + 1), 0) * halfExtent);
+	//		physx::PxRigidDynamic* body = Physics->createRigidDynamic(t.transform(localTm));
+	//		body->attachShape(*shape);
+	//		physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+	//		Scene->addActor(*body);
+	//	}
+	//}
+
+	Boxshape->release();
+	Sphereshape->release();
+	CapsuleShape->release();
+
 }
 
 void TestLevel_PhysX::Update(float _Delta)
