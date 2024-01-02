f1c781c8cbf9c4406b7ca42feffbaed62af91384
김태훈 <hyri0510@gmail.com>
2024-01-02 21:04:05 +0900

    김태훈 : 각 렌더유닛 OnOff 기능 추가

---
+++ DirectX_3DTeamPortfolio/GameEngineContents/CommonMonster.cpp
@@ -0,0 +1,25 @@
+#include "PreCompile.h"
+#include "CommonMonster.h"
+
+CommonMonster::CommonMonster() 
+{
+}
+
+CommonMonster::~CommonMonster() 
+{
+}
+
+void CommonMonster::Start()
+{
+	MainRenderer = CreateComponent<GameEngineFBXRenderer>(Enum_RenderOrder::Monster);
+	MainRenderer->SetMesh("Sphere");
+	MainRenderer->SetMaterial("FBXStaticColor");
+	MainRenderer->Transform.SetLocalScale(float4(50.0f, 50.0f, 50.0f));
+	MainRenderer->Transform.SetLocalRotation(float4(0.0f, 0.0f, -90.0f));
+}
+
+void CommonMonster::Release()
+{
+	MainRenderer = nullptr;
+}
+
--- DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_Monster.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_Monster.cpp
@@ -4,7 +4,6 @@
 
 #include "Monster_LothricKn.h"
 #include "Monster_HollowSoldier.h"
-#include "MonsterEditorActor.h"
 
 
 // _Level == TestLevel_Monster 
@@ -15,30 +14,53 @@ void MonsterGUI::OnGUI(GameEngineLevel* _Level, float _DeltaTime)
 		ShowLothricKnCount(_Level);
 	}
 
+	if (nullptr == CurLevel)
+	{
+		CurLevel = static_cast<TestLevel_Monster*>(_Level);
+	}
 
-	TestLevel_Monster* TestLevel = static_cast<TestLevel_Monster*>(_Level);
-	if (nullptr == TestLevel)
+	if (true == CObjectNames.empty())
+	{
+		CopyObjectName();
+	}
+
+	ShowObjectNameList();
+
+	if (nullptr == SelectActor)
 	{
 		return;
 	}
 
-	if (nullptr == TestLevel->EditorActor)
+	if (true == SelectActor->IsDeath())
 	{
+		SelectActor = nullptr;
 		return;
 	}
 
 	if (true == AnimationNames.empty())
 	{
-		CopyAnimationName(TestLevel);
+		CopyAnimationName();
 	}
 
 	if (ImGui::ListBox("Aniamtion Name", &SelectAnimationIndex, &CAnimationNames[0], static_cast<int>(CAnimationNames.size()), 8))
 	{
 		const char* SelectAnimationName = CAnimationNames.at(SelectAnimationIndex);
-		TestLevel->EditorActor->GetRenderer()->ChangeAnimation(SelectAnimationName);
-		CurAnimationInfo = TestLevel->EditorActor->GetRenderer()->GetAnimationInfos()[AnimationNames.at(SelectAnimationIndex)];
+		SelectActor->GetFBXRenderer()->ChangeAnimation(SelectAnimationName);
+		CurAnimationInfo = SelectActor->GetFBXRenderer()->GetAnimationInfos()[AnimationNames.at(SelectAnimationIndex)];
 	}
 
+	std::vector<std::vector<std::shared_ptr<GameEngineRenderUnit>>>& RenderUnits = SelectActor->GetFBXRenderer()->RenderUnits;
+	const int IndexCount = static_cast<int>(RenderUnits.size());
+	for (int i = 0; i < IndexCount; i++)
+	{
+		if (ImGui::Button(std::to_string(i).c_str()))
+		{
+			RenderUnits[i][0]->OnOffSwitch();
+		}
+	}
+
+
+	
 	if (bool TestCode = false)
 	{
 		if (ImGui::TreeNode("Mesh Editor"))
@@ -80,9 +102,47 @@ void MonsterGUI::ShowLothricKnCount(GameEngineLevel* _Level)
 	ImGui::Text(std::string("ObjectCount : " + std::to_string(MonsterCount)).c_str());
 }
 
-void MonsterGUI::CopyAnimationName(class TestLevel_Monster* _Level)
+void MonsterGUI::ShowObjectNameList()
 {
-	std::map<std::string, std::shared_ptr<GameEngineFBXAnimationInfo>>& Animations = _Level->EditorActor->GetRenderer()->GetAnimationInfos();
+	if (ImGui::ListBox("ActorList", &SelectActorIndex, &CObjectNames[0], static_cast<int>(CObjectNames.size())))
+	{
+		std::string ActorName = CObjectNames[SelectActorIndex];
+		if (true == ActorName.empty())
+		{
+			return;
+		}
+
+		const std::vector<std::shared_ptr<CommonMonster>>& MonsterGroup = CurLevel->GetObjectGroupConvert<CommonMonster>(Enum_UpdateOrder::Monster);
+		for (const std::shared_ptr<CommonMonster>& Monster : MonsterGroup)
+		{
+			if (CObjectNames[SelectActorIndex] == Monster->GetName())
+			{
+				SelectActor = Monster.get();
+			}
+		}
+	}
+}
+
+void MonsterGUI::CopyObjectName()
+{
+	const std::list<std::shared_ptr<GameEngineObject>>& ObjectGroup = CurLevel->GetObjectGroupInt(static_cast<int>(Enum_UpdateOrder::Monster));
+
+	CObjectNames.reserve(ObjectGroup.size());
+	ActorNames.reserve(ObjectGroup.size());
+	int Cnt = 0;
+
+	for (const std::shared_ptr<GameEngineObject>& Object : ObjectGroup)
+	{
+		ActorNames.push_back(Object->GetName());
+		CObjectNames.push_back(ActorNames[Cnt].c_str());
+		Cnt++;
+	}
+}
+
+
+void MonsterGUI::CopyAnimationName()
+{
+	std::map<std::string, std::shared_ptr<GameEngineFBXAnimationInfo>>& Animations = SelectActor->GetFBXRenderer()->GetAnimationInfos();
 	AnimationNames.reserve(Animations.size());
 	CAnimationNames.reserve(Animations.size());
 
@@ -96,14 +156,15 @@ void MonsterGUI::CopyAnimationName(class TestLevel_Monster* _Level)
 }
 
 
-
-
 void MonsterGUI::Release()
 {
 	CurAnimationInfo = nullptr;
 	AnimationNames.clear();
 	CAnimationNames.clear();
 }
+
+
+
 
 
 TestLevel_Monster::TestLevel_Monster() 
@@ -131,24 +192,16 @@ void TestLevel_Monster::Update(float _Delta)
 
 void TestLevel_Monster::LevelStart(GameEngineLevel* _PrevLevel)
 {
-	CreateActor<Monster_LothricKn>(Enum_UpdateOrder::Monster);
-	EditorActor = CreateActor<MonsterEditorActor>(Enum_UpdateOrder::Monster);
+	CreateActor<Monster_LothricKn>(static_cast<int>(Enum_UpdateOrder::Monster), "LothricKn");
 }
 
 void TestLevel_Monster::LevelEnd(GameEngineLevel* _NextLevel)
 {
 	AllDeathObjectGroupConvert<Monster_LothricKn>(Enum_UpdateOrder::Monster);
 
-	if (nullptr != EditorActor)
-	{
-		EditorActor->Death();
-		EditorActor = nullptr;
-	}
-
 	if (nullptr != MonsterWindow)
 	{
 		MonsterWindow->Release();
 		MonsterWindow->Off();
 	}
-	CreateActor<Monster_HollowSoldier>(Enum_UpdateOrder::Monster);
 }
