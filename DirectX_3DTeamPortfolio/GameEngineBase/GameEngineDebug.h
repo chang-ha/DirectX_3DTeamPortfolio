6b406f4ace4b9a3a8f33aceecd42728b50027867
chang-ha <kym637@naver.com>
2024-01-04 00:58:46 +0900

    우창하 : Transform 구조 개편 및 프리카메라 오류 수정

--- DirectX_3DTeamPortfolio/GameEngineBase/GameEngineDebug.h
+++ DirectX_3DTeamPortfolio/GameEngineBase/GameEngineDebug.h
@@ -12,3 +12,22 @@ namespace GameEngineDebug
 #define MsgBoxAssert(Text) \
 std::string Value = Text; \
 MessageBoxA(nullptr, Value.c_str(), "Error", MB_OK); assert(false);
+
+class DebugObject 
+{
+public:
+	void DebugOn() 
+	{
+		IsDebug = true;
+	}
+
+	void DebugOff()
+	{
+		IsDebug = false;
+	}
+
+	void DebugCheck();
+
+private:
+	bool IsDebug = false;
+};
