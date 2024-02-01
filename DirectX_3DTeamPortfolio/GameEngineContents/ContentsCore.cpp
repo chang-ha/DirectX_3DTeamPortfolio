e26aa1206c044334bf1b77308d446a1d501deff6
KimJunYoung <kimchk1104@naver.com>
2024-02-02 03:02:32 +0900

    김준영 : WorldMap 문제있음

--- DirectX_3DTeamPortfolio/GameEngineContents/ContentsCore.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/ContentsCore.cpp
@@ -43,11 +43,11 @@ void ContentsCore::Start()
 	
 	GameEngineCore::CreateLevel<TestLevel_Boss>("TestLevel_Boss");
 	GameEngineCore::CreateLevel<TestLevel_Monster>("TestLevel_Monster");
-	GameEngineCore::CreateLevel<TestLevel_Shader>("TestLevel_Shader");
+	GameEngineCore::CreateLevel<TestLevel_Shader>("TestLevel_Shader"); 
 	GameEngineCore::CreateLevel<TestLevel_Map>("TestLevel_Map");
 	GameEngineCore::CreateLevel<TestLevel_PhysX>("TestLevel_PhysX");
 	GameEngineCore::CreateLevel<PlayLevel>("PlayLevel");
-	GameEngineCore::ChangeLevel("PlayLevel");
+	GameEngineCore::ChangeLevel("TestLevel_Map");
 }
 
 void ContentsCore::Update(float _Delta)
