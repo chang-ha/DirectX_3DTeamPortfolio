c10492c381a7aa1880b06794a0d41ae729d7ef60
aruel7714 <harunia7714@daum.net>
2024-02-14 20:57:32 +0900

    김성원 : Monster_Hollow Animation Frame 조정

--- DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_Monster.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/TestLevel_Monster.cpp
@@ -6,7 +6,7 @@
 #include "ContentsLight.h"
 
 #include "Monster_LothricKn.h"
-#include "Monster_HollowSoldier_RoundShield.h"
+#include "Monster_HollowSoldier_Sword.h"
 #include "DummyActor.h"
 
 void MonsterGUITab::Init()
@@ -171,6 +171,7 @@ void TestLevel_Monster::Start()
 		Data.DifLightPower = 3.0f;
 		Data.SpcPow = 50.0f;
 		Data.AmbientLight = float4(0.4f);
+		Data.LightPower = 5.0f;
 
 		Test_Light1->SetLightData(Data);
 	}
@@ -214,11 +215,11 @@ void TestLevel_Monster::LevelStart(GameEngineLevel* _PrevLevel)
 	std::shared_ptr<Monster_LothricKn> LothricKn = CreateActor<Monster_LothricKn>(static_cast<int>(Enum_UpdateOrder::Monster), "LothricKn");
 	LothricKn->Transform.SetWorldPosition(float4(100.0f, 0.0f, 0.0f));
 
-	std::shared_ptr<Monster_HollowSoldier_RoundShield> Hollow = CreateActor<Monster_HollowSoldier_RoundShield>(static_cast<int>(Enum_UpdateOrder::Monster), "Hollow");
+	std::shared_ptr<Monster_HollowSoldier_Sword> Hollow = CreateActor<Monster_HollowSoldier_Sword>(static_cast<int>(Enum_UpdateOrder::Monster), "Hollow");
 	Hollow->Transform.SetWorldPosition(float4(100.0f, 0.0f, 0.0f));
 	//Hollow->Transform.SetWorldRotation(float4(0.0f, 180.0f, 0.0f));
 	Hollow->Transform.SetWorldRotation(float4(0.0f, 0.0f, 0.0f));
-	Hollow->SetStateIdle3();
+	Hollow->SetStateIdle1();
 
 	if (nullptr != pMonsterGUI)
 	{
@@ -229,7 +230,7 @@ void TestLevel_Monster::LevelStart(GameEngineLevel* _PrevLevel)
 void TestLevel_Monster::LevelEnd(GameEngineLevel* _NextLevel)
 {
 	AllDeathObjectGroupConvert<Monster_LothricKn>(Enum_UpdateOrder::Monster);
-	AllDeathObjectGroupConvert<Monster_HollowSoldier_RoundShield>(Enum_UpdateOrder::Monster);
+	AllDeathObjectGroupConvert<Monster_HollowSoldier_Sword>(Enum_UpdateOrder::Monster);
 	AllDeathObjectGroupConvert<DummyActor>(Enum_UpdateOrder::Monster);
 
 	if (nullptr != pMonsterGUI)
