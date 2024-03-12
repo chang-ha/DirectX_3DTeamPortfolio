76f64008beaf26368eb02f0babc8ffc600c476ab
chang-ha <kym637@naver.com>
2024-03-13 01:35:22 +0900

    우창하 : 보스 Move AI 추가

--- DirectX_3DTeamPortfolio/GameEngineContents/Boss_Vordt_State.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Boss_Vordt_State.cpp
@@ -400,25 +400,45 @@ void Boss_Vordt::StateInit()
 		Idle.Stay = std::bind(&Boss_Vordt::Idle_Update, this, std::placeholders::_1);
 		Idle.End = std::bind(&Boss_Vordt::Idle_End, this);
 
+		mJumpTableManager.AddJumpTable("Idle", 0, 120, std::bind(&Boss_Vordt::AI_Combo, this));
+		mJumpTableManager.AddJumpTable("Idle", 0, 120, std::bind(&Boss_Vordt::AI_Attack, this));
+		mJumpTableManager.AddJumpTable("Idle", 0, 120, std::bind(&Boss_Vordt::AI_Dodge, this));
+
 		CreateStateParameter Walk_Front;
 		Walk_Front.Start = std::bind(&Boss_Vordt::Walk_Front_Start, this);
 		Walk_Front.Stay = std::bind(&Boss_Vordt::Walk_Front_Update, this, std::placeholders::_1);
 		Walk_Front.End = std::bind(&Boss_Vordt::Walk_Front_End, this);
+
+		mJumpTableManager.AddJumpTable("Walk_Front", 0, 108, std::bind(&Boss_Vordt::AI_Combo, this));
+		mJumpTableManager.AddJumpTable("Walk_Front", 0, 108, std::bind(&Boss_Vordt::AI_Attack, this));
+		mJumpTableManager.AddJumpTable("Walk_Front", 0, 108, std::bind(&Boss_Vordt::AI_Dodge, this));
 
 		CreateStateParameter Walk_Right;
 		Walk_Right.Start = std::bind(&Boss_Vordt::Walk_Right_Start, this);
 		Walk_Right.Stay = std::bind(&Boss_Vordt::Walk_Right_Update, this, std::placeholders::_1);
 		Walk_Right.End = std::bind(&Boss_Vordt::Walk_Right_End, this);
 
+		mJumpTableManager.AddJumpTable("Walk_Right", 0, 108, std::bind(&Boss_Vordt::AI_Combo, this));
+		mJumpTableManager.AddJumpTable("Walk_Right", 0, 108, std::bind(&Boss_Vordt::AI_Attack, this));
+		mJumpTableManager.AddJumpTable("Walk_Right", 0, 108, std::bind(&Boss_Vordt::AI_Dodge, this));
+
 		CreateStateParameter Walk_Left;
 		Walk_Left.Start = std::bind(&Boss_Vordt::Walk_Left_Start, this);
 		Walk_Left.Stay = std::bind(&Boss_Vordt::Walk_Left_Update, this, std::placeholders::_1);
 		Walk_Left.End = std::bind(&Boss_Vordt::Walk_Left_End, this);
 
+		mJumpTableManager.AddJumpTable("Walk_Left", 0, 108, std::bind(&Boss_Vordt::AI_Combo, this));
+		mJumpTableManager.AddJumpTable("Walk_Left", 0, 108, std::bind(&Boss_Vordt::AI_Attack, this));
+		mJumpTableManager.AddJumpTable("Walk_Left", 0, 108, std::bind(&Boss_Vordt::AI_Dodge, this));
+
 		CreateStateParameter Rush_Front;
 		Rush_Front.Start = std::bind(&Boss_Vordt::Rush_Front_Start, this);
 		Rush_Front.Stay = std::bind(&Boss_Vordt::Rush_Front_Update, this, std::placeholders::_1);
 		Rush_Front.End = std::bind(&Boss_Vordt::Rush_Front_End, this);
+
+		mJumpTableManager.AddJumpTable("Rush_Front", 0, 22, std::bind(&Boss_Vordt::AI_Combo, this));
+		mJumpTableManager.AddJumpTable("Rush_Front", 0, 22, std::bind(&Boss_Vordt::AI_Attack, this));
+		mJumpTableManager.AddJumpTable("Rush_Front", 0, 22, std::bind(&Boss_Vordt::AI_Dodge, this));
 
 		CreateStateParameter Jump_Back;
 		Jump_Back.Start = std::bind(&Boss_Vordt::Jump_Back_Start, this);
@@ -1154,19 +1174,125 @@ Enum_JumpTableFlag Boss_Vordt::AI_MoveMent()
 {
 	// Move to Target
 	//// List of MoveMent
-	//	1. Walk_Front
-	//	2. Walk_Left
-	//	3. Walk_Right
-	//	4. Rush_Front
-	//	5. Turn_Left
-	//	6. Turn_Right
-	//	7. Turn_Left_Twice
-	//	8. Turn_Right_Twice
+	//	1. Walk_Front // tDis > Enum_TargetDis::Dis_Close && tDeg == Enum_TargetDeg::Deg_Front
+	//	2. Walk_Left // tDis > Enum_TargetDis::Dis_Close && tDeg == Enum_TargetDeg::Deg_Side && Dir == Enum_RotDir::Left
+	//	3. Walk_Right // tDis > Enum_TargetDis::Dis_Close && tDeg == Enum_TargetDeg::Deg_Side && Dir == Enum_RotDir::Right
+	//	4. Rush_Front // tDis == Enum_TargetDis::Dis_Far && tDeg == Enum_TargetDeg::Deg_Front
+	//	5. Turn_Left // tDis < Enum_TargetDis::Dis_Middle && tDeg == Enum_TargetDeg::Deg_Side && Dir == Enum_RotDir::Left
+	//	6. Turn_Right // tDis < Enum_TargetDis::Dis_Middle && tDeg == Enum_TargetDeg::Deg_Side && Dir == Enum_RotDir::Right
+	//	7. Turn_Left_Twice // tDis < Enum_TargetDis::Dis_Middle && tDeg == Enum_TargetDeg::Deg_Back && Dir == Enum_RotDir::Left
+	//	8. Turn_Right_Twice // tDis < Enum_TargetDis::Dis_Middle && tDeg == Enum_TargetDeg::Deg_Back && Dir == Enum_RotDir::Right
+
 
 	int CurState = MainState.GetCurState();
 	int tDis = mTargetState.mTargetDis;
 	int tDeg = mTargetState.mTargetDeg;
+	Enum_RotDir Dir = GetRotDir_e();
 
+	switch (tDeg)
+	{
+	case Enum_TargetDeg::Deg_Front:
+	{
+		if (tDis == Enum_TargetDis::Dis_Close)
+		{
+			break;
+		}
+
+		if (tDis == Enum_TargetDis::Dis_Far)
+		{
+			if (true == ChangeAI_State(Enum_BossState::Rush_Front))
+			{
+				return Enum_JumpTableFlag::StopJumpTable;
+			}
+		}
+
+		if (true == ChangeAI_State(Enum_BossState::Walk_Front))
+		{
+			return Enum_JumpTableFlag::StopJumpTable;
+		}
+
+		break;
+	}
+	case Enum_TargetDeg::Deg_Side:
+	{
+		switch (Dir)
+		{
+		case Left:
+		{
+			if (tDis < Enum_TargetDis::Dis_Middle)
+			{
+				if (true == ChangeAI_State(Enum_BossState::Turn_Left))
+				{
+					return Enum_JumpTableFlag::StopJumpTable;
+				}
+			}
+			else
+			{
+				if (true == ChangeAI_State(Enum_BossState::Walk_Left))
+				{
+					return Enum_JumpTableFlag::StopJumpTable;
+				}
+			}
+			break;
+		}
+		case Right:
+		{
+			if (tDis < Enum_TargetDis::Dis_Middle)
+			{
+				if (true == ChangeAI_State(Enum_BossState::Turn_Right))
+				{
+					return Enum_JumpTableFlag::StopJumpTable;
+				}
+			}
+			else
+			{
+				if (true == ChangeAI_State(Enum_BossState::Walk_Right))
+				{
+					return Enum_JumpTableFlag::StopJumpTable;
+				}
+			}
+			break;
+		}
+		case Not_Rot:
+		default:
+			break;
+		}
+		break;
+	}
+	case Enum_TargetDeg::Deg_Back:
+	{
+		if (tDis > Enum_TargetDis::Dis_Close)
+		{
+			break;
+		}
+
+		switch (Dir)
+		{
+		case Left:
+		{
+			if (true == ChangeAI_State(Enum_BossState::Turn_Left_Twice))
+			{
+				return Enum_JumpTableFlag::StopJumpTable;
+			}
+			break;
+		}
+		case Right:
+		{
+			if (true == ChangeAI_State(Enum_BossState::Turn_Right_Twice))
+			{
+				return Enum_JumpTableFlag::StopJumpTable;
+			}
+			break;
+		}
+		case Not_Rot:
+		default:
+			break;
+		}
+		break;
+	}
+	default:
+		break;
+	}
 
 	return Enum_JumpTableFlag::Default;
 }
