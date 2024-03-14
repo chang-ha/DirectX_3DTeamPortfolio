c9fb3d46e239525c472a660af8cd9f748a373414
chang-ha <kym637@naver.com>
2024-03-14 15:39:29 +0900

    우창하 : 업데이트

--- DirectX_3DTeamPortfolio/GameEngineContents/Boss_HitInteraction.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Boss_HitInteraction.cpp
@@ -11,3 +11,13 @@ Boss_HitInteraction::~Boss_HitInteraction()
 {
 
 }
+
+void Boss_HitInteraction::CollisionToBody(Enum_CollisionOrder _Order, int _iStiffness /*= 0*/) 
+{
+
+}
+
+void Boss_HitInteraction::CollisionToShield(Enum_CollisionOrder _Order, int _iStiffness /*= 0*/) 
+{
+
+}
