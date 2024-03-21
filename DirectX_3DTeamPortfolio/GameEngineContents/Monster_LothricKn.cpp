341e7fd93c1cbbf7b348cc22253aebecf6b4127e
김태훈 <hyri0510@gmail.com>
2024-03-22 00:59:57 +0900

    김태훈 : 로스릭 피직스 크기 수정

--- DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Monster_LothricKn.cpp
@@ -22,11 +22,6 @@ void Monster_LothricKn::Start()
 {
 	BaseMonster::Start();
 
-	if (nullptr != Capsule)
-	{
-		Capsule->PhysXComponentInit(100.0f, 100.0f);
-	}
-
 	// Outer Res
 	LoadRes3DSound("c128001001.wav");
 	BaseActor::PushMaterialSound(Enum_DS3MaterialSound::E1001, "c128001001.wav");
