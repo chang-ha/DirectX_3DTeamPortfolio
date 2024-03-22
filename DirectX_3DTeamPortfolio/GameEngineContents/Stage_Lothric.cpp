80bcd40ad7e809079ea2dcef82165daf77f6e846
botteyu1 <botteyu1@gmail.com>
2024-03-22 20:26:17 +0900

    유준상: 안개 밑바닥 삭제

--- DirectX_3DTeamPortfolio/GameEngineContents/Stage_Lothric.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Stage_Lothric.cpp
@@ -74,16 +74,7 @@ Stage_Lothric::~Stage_Lothric()
 
 void Stage_Lothric::LevelStart(GameEngineLevel* _PrevLevel)
 {
-	{
-		std::shared_ptr<GameEngineActor> Ground = CreateActor<GameEngineActor>(0);
-		std::shared_ptr<GameEngineRenderer> NewRenderer = Ground->CreateComponent<GameEngineRenderer>();
-		NewRenderer->SetMesh("Box");
-		NewRenderer->SetMaterial("FBX_Static_Color");
-		// NewRenderer->GetShaderResHelper().SetTexture("NormalTexture", "BumpNormal.gif");
-		NewRenderer->Transform.SetLocalPosition({ 0.0f, -40000.0f, 0.0f });
-		NewRenderer->Transform.SetLocalScale({ 1000000.0f, 100.0f, 1000000.0f });
-		NewRenderer->RenderBaseInfoValue.BaseColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-	}
+	
 
 	if (nullptr == Boss_Object)
 	{
