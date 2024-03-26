a802aabb40d3be9e0953fc82b30c819677ef1255
김태훈 <hyri0510@gmail.com>
2024-03-26 15:14:06 +0900

    김태훈 : 레벨 State 변경

--- DirectX_3DTeamPortfolio/GameEngineContents/Stage_Lothric_State.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/Stage_Lothric_State.cpp
@@ -15,36 +15,20 @@
 #include "Object_FogWall.h"
 #include "Object_bonfire.h"
 
-void NUllCheck(void* _Pointer, std::string_view _MsgBox = "")
-{
-	if (nullptr == _Pointer)
-	{
-		if (_MsgBox.empty())
-		{
-			MsgBoxAssert("포인터가 존재하지 않습니다.");
-		}
-		else
-		{
-			MsgBoxAssert(_MsgBox.data());
-		}
-	}
-}
 
 void Stage_Lothric::StateInit()
 {
 	LevelState.CreateState(Enum_LevelState::Play, {                .Start = std::bind(&Stage_Lothric::Start_Play,this, std::placeholders::_1),                .Stay = std::bind(&Stage_Lothric::Update_Play,this, std::placeholders::_1,std::placeholders::_2),           .End = std::bind(&Stage_Lothric::End_Play, this, std::placeholders::_1)           });
 	LevelState.CreateState(Enum_LevelState::PlayerDeath, {         .Start = std::bind(&Stage_Lothric::Start_PlayerDeath,this, std::placeholders::_1),         .Stay = std::bind(&Stage_Lothric::Update_PlayerDeath,this, std::placeholders::_1,std::placeholders::_2),    .End = std::bind(&Stage_Lothric::End_PlayerDeath, this, std::placeholders::_1)    });
-	LevelState.CreateState(Enum_LevelState::BossDeath, {         .Start = std::bind(&Stage_Lothric::Start_BossDeath,this, std::placeholders::_1),         .Stay = std::bind(&Stage_Lothric::Update_BossDeath,this, std::placeholders::_1,std::placeholders::_2),    .End = std::bind(&Stage_Lothric::End_BossDeath, this, std::placeholders::_1)    });
-	LevelState.CreateState(Enum_LevelState::StageClear, {          .Start = std::bind(&Stage_Lothric::Start_StageClear,this, std::placeholders::_1),          .Stay = std::bind(&Stage_Lothric::Update_StageClear,this, std::placeholders::_1,std::placeholders::_2),     .End = std::bind(&Stage_Lothric::End_StageClear, this, std::placeholders::_1)     });
 
 	PlayerDeathState.CreateState(Enum_PlayerDeathState::Ready, {   .Start = std::bind(&Stage_Lothric::Start_PlayerDeath_Ready,this, std::placeholders::_1),   .Stay = std::bind(&Stage_Lothric::Update_PlayerDeath_Ready,this, std::placeholders::_1,std::placeholders::_2)                                                                                 });
 	PlayerDeathState.CreateState(Enum_PlayerDeathState::YouDie, {  .Start = std::bind(&Stage_Lothric::Start_PlayerDeath_YouDie,this, std::placeholders::_1),  .Stay = std::bind(&Stage_Lothric::Update_PlayerDeath_YouDie,this, std::placeholders::_1,std::placeholders::_2)                                                                                });
 	PlayerDeathState.CreateState(Enum_PlayerDeathState::FadeOut, { .Start = std::bind(&Stage_Lothric::Start_PlayerDeath_FadeOut,this, std::placeholders::_1), .Stay = std::bind(&Stage_Lothric::Update_PlayerDeath_FadeOut,this, std::placeholders::_1,std::placeholders::_2)                                                                               });
 	PlayerDeathState.CreateState(Enum_PlayerDeathState::Done, {                                                                                                                                                                                                                                                                                             });
 
-	BossDeathState.CreateState(Enum_BossDeathState::Ready, {   .Start = std::bind(&Stage_Lothric::Start_BossDeath_Ready,this, std::placeholders::_1),   .Stay = std::bind(&Stage_Lothric::Update_BossDeath_Ready,this, std::placeholders::_1,std::placeholders::_2)                                                                                 });
-	BossDeathState.CreateState(Enum_BossDeathState::Alert, { .Start = std::bind(&Stage_Lothric::Start_BossDeath_Alert,this, std::placeholders::_1),  .Stay = std::bind(&Stage_Lothric::Update_BossDeath_Alert,this, std::placeholders::_1,std::placeholders::_2) });
-	BossDeathState.CreateState(Enum_BossDeathState::Done, {                                                                                                                                                                                                                                                                                             });
+	BossStageState.CreateState(Enum_BossStageState::Ready, { .Start = std::bind(&Stage_Lothric::Start_BossStage_Ready,this, std::placeholders::_1),   .Stay = std::bind(&Stage_Lothric::Update_BossStage_Ready,this, std::placeholders::_1,std::placeholders::_2) });
+	BossStageState.CreateState(Enum_BossStageState::Fight, { .Start = std::bind(&Stage_Lothric::Start_BossStage_Fight,this, std::placeholders::_1),   .Stay = std::bind(&Stage_Lothric::Update_BossStage_Fight,this, std::placeholders::_1,std::placeholders::_2) });
+	BossStageState.CreateState(Enum_BossStageState::Clear, { .Start = std::bind(&Stage_Lothric::Start_BossStage_Clear,this, std::placeholders::_1),   .Stay = std::bind(&Stage_Lothric::Update_BossStage_Clear,this, std::placeholders::_1,std::placeholders::_2) });
 }
 
 #pragma region PlayLevel State
@@ -63,17 +47,10 @@ void Stage_Lothric::Update_Play(float _Delta, GameEngineState* _Parent)
 {
 	PlayUpdate(_Delta);
 
-	NUllCheck(Player_Object.get(), "플레이어가 존재하지 않습니다. 값을 확인해주세요.");
+	ContentsDebug::NUllCheck(Player_Object.get(), "플레이어가 존재하지 않습니다. 값을 확인해주세요.");
 	if (true == Player_Object->IsFlag(Enum_ActorFlag::Death))
 	{
 		_Parent->ChangeState(Enum_LevelState::PlayerDeath);
-		return;
-	}
-
-	NUllCheck(Boss_Object.get(), "보스가 존재하지 않습니다. 값을 확인해주세요.");
-	if (true == Boss_Object->IsFlag(Enum_ActorFlag::Death))
-	{
-		_Parent->ChangeState(Enum_LevelState::BossDeath);
 		return;
 	}
 }
@@ -111,55 +88,6 @@ void Stage_Lothric::End_PlayerDeath(GameEngineState* _Parent)
 
 }
 
-// BossDeath State
-void Stage_Lothric::Start_BossDeath(GameEngineState* _Parent)
-{
-	BossDeathState.ChangeState(Enum_PlayerDeathState::Ready);
-	if (nullptr != BossRoom_bonfire && false == BossRoom_bonfire->IsUpdate())
-	{
-		BossRoom_bonfire->On();
-	}
-	
-	if (nullptr != FogWall)
-	{
-		FogWall->Death();
-		FogWall = nullptr;
-	}
-}
-
-void Stage_Lothric::Update_BossDeath(float _Delta, GameEngineState* _Parent)
-{
-	BossDeathState.Update(_Delta);
-
-	bool BossDeathStateDone = (Enum_BossDeathState::Done == static_cast<Enum_BossDeathState>(BossDeathState.GetCurState()));
-	if (BossDeathStateDone)
-	{
-		_Parent->ChangeState(Enum_LevelState::StageClear);
-		return;
-	}
-}
-
-void Stage_Lothric::End_BossDeath(GameEngineState* _Parent)
-{
-
-}
-
-// StageClear State
-void Stage_Lothric::Start_StageClear(GameEngineState* _Parent)
-{
-
-}
-
-void Stage_Lothric::Update_StageClear(float _Delta, GameEngineState* _Parent)
-{
-
-}
-
-void Stage_Lothric::End_StageClear(GameEngineState* _Parent)
-{
-
-}
-
 #pragma endregion
 
 #pragma region PlayerDeath State
@@ -182,7 +110,7 @@ void Stage_Lothric::Update_PlayerDeath_Ready(float _Delta, GameEngineState* _Par
 // YouDie State
 void Stage_Lothric::Start_PlayerDeath_YouDie(GameEngineState* _Parent)
 {
-	NUllCheck(MainUI.get());
+	ContentsDebug::NUllCheck(MainUI.get());
 	UIAlertMaanger::CallAlert(Enum_AlertType::YouDie);
 }
 
@@ -198,13 +126,13 @@ void Stage_Lothric::Update_PlayerDeath_YouDie(float _Delta, GameEngineState* _Pa
 // FadeOut State
 void Stage_Lothric::Start_PlayerDeath_FadeOut(GameEngineState* _Parent)
 {
-	NUllCheck(FadeObject.get());
+	ContentsDebug::NUllCheck(FadeObject.get());
 	FadeObject->FadeOut();
 }
 
 void Stage_Lothric::Update_PlayerDeath_FadeOut(float _Delta, GameEngineState* _Parent)
 {
-	NUllCheck(FadeObject.get());
+	ContentsDebug::NUllCheck(FadeObject.get());
 	bool FadeDone = (false == FadeObject->IsUpdate());
 	if (FadeDone)
 	{
@@ -215,40 +143,61 @@ void Stage_Lothric::Update_PlayerDeath_FadeOut(float _Delta, GameEngineState* _P
 
 #pragma endregion
 
-#pragma region Boss Death State
+#pragma region Boss Stage State
 
-void Stage_Lothric::Start_BossDeath_Ready(GameEngineState* _Parent)
+// Ready State
+void Stage_Lothric::Start_BossStage_Ready(GameEngineState* _Parent)
+{
+
+}
+
+void Stage_Lothric::Update_BossStage_Ready(float _Delta, GameEngineState* _Parent)
+{
+
+}
+
+
+// Fight State
+void Stage_Lothric::Start_BossStage_Fight(GameEngineState* _Parent)
 {
 	MainUIActor::SoundPlay("Destroyed_Sound_Effect.wav");
 }
 
-void Stage_Lothric::Update_BossDeath_Ready(float _Delta, GameEngineState* _Parent)
+void Stage_Lothric::Update_BossStage_Fight(float _Delta, GameEngineState* _Parent)
+{
+	ContentsDebug::NUllCheck(Boss_Object.get(), "플레이어가 존재하지 않습니다. 값을 확인해주세요.");
+	if (true == Boss_Object->IsFlag(Enum_ActorFlag::Death))
+	{
+		_Parent->ChangeState(Enum_BossStageState::Clear);
+		return;
+	}
+}
+
+// Clear State
+void Stage_Lothric::Start_BossStage_Clear(GameEngineState* _Parent)
+{
+	if (nullptr != BossRoom_bonfire && false == BossRoom_bonfire->IsUpdate())
+	{
+		BossRoom_bonfire->On();
+	}
+
+	if (nullptr != FogWall)
+	{
+		FogWall->Death();
+		FogWall = nullptr;
+	}
+}
+
+void Stage_Lothric::Update_BossStage_Clear(float _Delta, GameEngineState* _Parent)
 {
 	if (_Parent->GetStateTime() > 3.0f)
 	{
-		_Parent->ChangeState(Enum_BossDeathState::Alert);
+		UIAlertMaanger::CallAlert(Enum_AlertType::Destroy);
+		UIAlertMaanger::CallAlert(Enum_AlertType::BoneFire);
+		_Parent->ChangeState(Enum_BossStageState::Ready);
 		return;
 	}
 }
 
-
-void Stage_Lothric::Start_BossDeath_Alert(GameEngineState* _Parent)
-{
-	UIAlertMaanger::CallAlert(Enum_AlertType::Destroy);
-	UIAlertMaanger::CallAlert(Enum_AlertType::BoneFire);
-
-	IsAlert = false;
-}
-
-void Stage_Lothric::Update_BossDeath_Alert(float _Delta, GameEngineState* _Parent)
-{
-	if (false == UIAlertMaanger::Alerting && true == IsAlert)
-	{
-		_Parent->ChangeState(Enum_BossDeathState::Done);
-		return;
-	}
-
-	IsAlert = true;
-}
 
 #pragma endregion
