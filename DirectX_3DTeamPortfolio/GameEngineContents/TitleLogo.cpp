bb10ec6c0fd6a649044cd66c28aeaa45c7f09e2c
dudgus614@naver.com <dudgus614@naver.com>
2024-03-27 17:32:40 +0900

    홍영현 : 타이틀로고 구조 변경중

--- DirectX_3DTeamPortfolio/GameEngineContents/TitleLogo.cpp
+++ DirectX_3DTeamPortfolio/GameEngineContents/TitleLogo.cpp
@@ -5,6 +5,8 @@
 
 #include "MainUIActor.h"
 
+#include "UI_Utils.h"
+
 TitleLogo::TitleLogo()
 {
 
@@ -15,78 +17,176 @@ TitleLogo::~TitleLogo()
 
 }
 
+#define ButtonFontStartYPos -84.0f
+#define ButtonYDistance 32.0f
+
+#define ButtonBackStartYPos -92.5f
+#define ButtonBackYDistance -32
+
 void TitleLogo::Start()
 {
-	
 	float4 WindowScale = GameEngineCore::MainWindow.GetScale();
 
-	BanDaiNamco_Logo = CreateComponent<GameEngineSpriteRenderer>();
+	BanDaiNamco_Logo = CreateComponent<GameEngineSpriteRenderer>(Enum_RenderOrder::UI);
 	BanDaiNamco_Logo->SetSprite("BanDaiNaco_Logo.Png");
 	BanDaiNamco_Logo->SetImageScale({ WindowScale.X , WindowScale.Y });
 	BanDaiNamco_Logo->Transform.SetLocalPosition({ 0.0f, 0.0f, 500.0f });
 	BanDaiNamco_Logo->On();
 
-	FromSoft_Logo = CreateComponent<GameEngineSpriteRenderer>();
+	FromSoft_Logo = CreateComponent<GameEngineSpriteRenderer>(Enum_RenderOrder::UI);
 	FromSoft_Logo->SetSprite("FromSoftLogo.Png");
 	FromSoft_Logo->SetImageScale({ 1024, 1024 });
 	FromSoft_Logo->Transform.SetLocalPosition({ 0.0f, 0.0f, 300.0f });
 	FromSoft_Logo->GetColorData().MulColor.A = 0.0f;
 
-	DarkSouls_Logo = CreateComponent<GameEngineSpriteRenderer>();
+	DarkSouls_Logo = CreateComponent<GameEngineSpriteRenderer>(Enum_RenderOrder::UI);
 	DarkSouls_Logo->SetSprite("DarkSoulsLogo.Png");
 	DarkSouls_Logo->AutoSpriteSizeOn();
 	DarkSouls_Logo->Transform.SetLocalPosition({ 0.0f, 50.0f, 500.0f });
 	DarkSouls_Logo->GetColorData().MulColor.A = 0.0f;
 
-	Inc_Logo = CreateComponent<GameEngineSpriteRenderer>();
+	Inc_Logo = CreateComponent<GameEngineSpriteRenderer>(Enum_RenderOrder::UI);
 	Inc_Logo->SetSprite("Title_IncLogo.Png");
 	Inc_Logo->AutoSpriteSizeOn();
 	Inc_Logo->Transform.SetLocalPosition({ 0.0f, -250.0f, 500.0f });
 	Inc_Logo->Off();
 
-	AnyButtonBack = CreateComponent<GameEngineSpriteRenderer>();
+	AnyButtonBack = CreateComponent<GameEngineSpriteRenderer>(Enum_RenderOrder::UI_BackTexture);
 	AnyButtonBack->SetSprite("AnyButtonBack.Png");
 	AnyButtonBack->AutoSpriteSizeOn();
 	AnyButtonBack->SetAutoScaleRatio(0.45f);
-	//AnyButtonBack->Transform.SetLocalPosition({ 0.0f, -119.95f, 505.0f });
 	AnyButtonBack->Transform.SetLocalPosition({ 0.0f, -120.0f, 505.0f });
 	AnyButtonBack->GetColorData().MulColor.A = 0.0f;
 
-	FontRender = CreateComponent<GameEngineUIRenderer>();
-	FontRender->SetText(GlobalValue::OptimusFont, "PRESS ANY BUTTON", FontScale, float4{ 1, 1, 1, 1 }, FW1_CENTER);
-	FontRender->Transform.SetLocalPosition({ 0.0f, -111.0f, 500.0f });
-	FontRender->Off();
+	AnyButtonFont = CreateComponent<GameEngineUIRenderer>(Enum_RenderOrder::UI_Font);
+	AnyButtonFont->SetText(GlobalValue::OptimusFont, "PRESS ANY BUTTON", FontScale, float4{ 1, 1, 1, 1 }, FW1_CENTER);
+	AnyButtonFont->Transform.SetLocalPosition({ 0.0f, -111.0f, 500.0f });
+	AnyButtonFont->Off();
 
 	GameEngineInput::AddInputObject(this);
+
+	///// State
+	CreateStateParameter BanDaiState;
+	BanDaiState.Stay = std::bind(&TitleLogo::Update_BanDai, this, std::placeholders::_1, std::placeholders::_2);
+	BanDaiState.End = std::bind(&TitleLogo::End_BanDai, this, std::placeholders::_1);
+
+	CreateStateParameter FromSoftState;
+	FromSoftState.Stay = std::bind(&TitleLogo::Update_FromSoft, this, std::placeholders::_1, std::placeholders::_2);
+	FromSoftState.End = std::bind(&TitleLogo::End_FromSoft, this, std::placeholders::_1);
+
+	CreateStateParameter DarkSoulState;
+	DarkSoulState.Stay = std::bind(&TitleLogo::Update_DarkSoul, this, std::placeholders::_1, std::placeholders::_2);
+	DarkSoulState.End = std::bind(&TitleLogo::End_DarkSoul, this, std::placeholders::_1);
+
+	CreateStateParameter AnyButtonState;
+	AnyButtonState.Stay = std::bind(&TitleLogo::Update_AnyButton, this, std::placeholders::_1, std::placeholders::_2);
+	AnyButtonState.End = std::bind(&TitleLogo::End_AnyButton, this, std::placeholders::_1);
+
+
+	CreateStateParameter DoneState;
+	DoneState.Start = std::bind(&TitleLogo::Start_Done, this, std::placeholders::_1);
+
+
+	Logostate.CreateState(eLogoState::BanDai, BanDaiState);
+	Logostate.CreateState(eLogoState::FromSoft, FromSoftState);
+	Logostate.CreateState(eLogoState::DarkSoul, DarkSoulState);
+	Logostate.CreateState(eLogoState::AnyButton, AnyButtonState);
+	Logostate.CreateState(eLogoState::Done, DoneState);
+
+
+	Logostate.ChangeState(eLogoState::BanDai);
+
+	// 버튼만들기
+	ButtonCreate();
+
+	CreateStateParameter BStartButton;
+
+
+	/*UtilRender = AnyButtonBack;
+	UIHandler::Update(_Delta, UtilRender.get(), this);*/
 }
 
 void TitleLogo::Update(float _Delta)
 {
-	Time += _Delta;
-	if (Time >= 2.0f)
+	Logostate.Update(_Delta);
+
+	ButtonState.Update(_Delta);
+}
+
+void TitleLogo::Release()
+{
+	DarkBack = nullptr;
+	BanDaiNamco_Logo = nullptr;
+	FromSoft_Logo = nullptr;
+	DarkSouls_Logo = nullptr;
+	Inc_Logo = nullptr;
+	AnyButtonBack = nullptr;
+	AnyButtonFont = nullptr;
+
+
+}
+
+// 버튼 만들기
+void TitleLogo::ButtonCreate()
+{
+	ButtonFont.reserve(2);
+	for (int i = 0; i < ButtonFont.capacity(); i++)
+	{
+		std::shared_ptr<GameEngineUIRenderer> Button = CreateComponent<GameEngineUIRenderer>(Enum_RenderOrder::UI_Font);
+		Button->SetText(GlobalValue::OptimusFont, "버튼 이름", FontScale, float4::WHITE, FW1_CENTER);
+		Button->Transform.SetLocalPosition(float4(0.0f, ButtonFontStartYPos - (i * ButtonYDistance)));
+		Button->Off();
+		ButtonFont.push_back(Button);
+	}
+
+	ButtonFont[GameStart]->ChangeText("START BUTTON");
+	ButtonFont[GameExit]->ChangeText("EXIT BUTTON");
+}
+
+
+// State
+constexpr float LogoChangeTime = 4.0f;
+void TitleLogo::Update_BanDai(float _Delta, GameEngineState* _State)
+{
+	SkipButton(_State, (eLogoState::FromSoft));
+
+	if (_State->GetStateTime() >= LogoChangeTime)
+	{
+		_State->ChangeState(eLogoState::FromSoft);
+		return;
+	}
+
+	if (_State->GetStateTime() >= 2.0f)
 	{
 		BanDaiNamco_Logo->GetColorData().MulColor.A -= _Delta * 0.5f;
 	}
+}
 
-	if (Time >= FromSoftLogoTime)
+void TitleLogo::Update_FromSoft(float _Delta, GameEngineState* _State)
+{
+	SkipButton(_State, (eLogoState::DarkSoul));
+
+	if (_State->GetStateTime() >= LogoChangeTime)
 	{
-		BanDaiNamco_Logo->Off();
-		FromSoft_Logo->GetColorData().MulColor.A += _Delta * 0.5f;
+		_State->ChangeState(eLogoState::DarkSoul);
+		return;
 	}
 
-	if (Time >= 8.0f)
-	{
-		FromSoft_Logo->Off();
-		DarkSouls_Logo->GetColorData().MulColor.A += _Delta * 0.5f;
-	}
+	FromSoft_Logo->GetColorData().MulColor.A += _Delta * 0.5f;
+}
+void TitleLogo::Update_DarkSoul(float _Delta, GameEngineState* _State)
+{
+	SkipButton(_State, (eLogoState::AnyButton));
 
-	if (DarkSouls_Logo->GetColorData().MulColor.A >= 1.0f && AnyBack == false)
+	DarkSouls_Logo->GetColorData().MulColor.A += _Delta * 0.5f;
+	if (_State->GetStateTime() >= LogoChangeTime)
 	{
-		Inc_Logo->On();
-		FontRender->On();
-		AnyBack = true;
+		_State->ChangeState(eLogoState::AnyButton);
+		return;
 	}
-
+}
+void TitleLogo::Update_AnyButton(float _Delta, GameEngineState* _State)
+{
 	if (AnyBack == true && AnyBackColor == false)
 	{
 		AnyButtonBack->GetColorData().MulColor.A += _Delta * 0.5f;
@@ -113,25 +213,76 @@ void TitleLogo::Update(float _Delta)
 	if (FontEnter == true)
 	{
 		FontScale += 4.0f * _Delta;
-		FontRender->Transform.AddLocalPosition(float4(0.0f, 2.0f * _Delta));
-		FontRender->SetText(GlobalValue::OptimusFont, "PRESS ANY BUTTON", FontScale, float4{ 1, 1, 1, 1 }, FW1_CENTER);
+		AnyButtonFont->Transform.AddLocalPosition(float4(0.0f, 2.0f * _Delta));
+		AnyButtonFont->SetText(GlobalValue::OptimusFont, "PRESS ANY BUTTON", FontScale, float4{ 1, 1, 1, 1 }, FW1_CENTER);
 		AnyButtonBack->GetColorData().MulColor.A += _Delta * 0.5f;
 	}
 
 	if (FontScale >= 25.0f)
 	{
-		LoadingLevel::LoadingFlag = Enum_LevelFlag::Loading_Resouce;
-		GameEngineCore::ChangeLevel("LoadingLevel");
+		_State->ChangeState(eLogoState::Done);
+		ButtonState.ChangeState(eButtonState::BStart);
+		return;
 	}
 }
 
-void TitleLogo::Release()
+
+
+
+void TitleLogo::End_BanDai(GameEngineState* _State)
 {
-	DarkBack = nullptr;
-	BanDaiNamco_Logo = nullptr;
-	FromSoft_Logo = nullptr;
-	DarkSouls_Logo = nullptr;
-	Inc_Logo = nullptr;
-	FontRender = nullptr;
-	AnyButtonBack = nullptr;
+	BanDaiNamco_Logo->Off();
+}
+void TitleLogo::End_FromSoft(GameEngineState* _State)
+{
+	FromSoft_Logo->Off();
+}
+void TitleLogo::End_DarkSoul(GameEngineState* _State)
+{
+	Inc_Logo->On();
+	AnyButtonFont->On();
+	AnyBack = true;
+
+	DarkSouls_Logo->GetColorData().MulColor.A = 1.0f;
+}
+void TitleLogo::End_AnyButton(GameEngineState* _State)
+{
+	AnyButtonFont->Off();
+}
+
+void TitleLogo::Start_Done(GameEngineState* _State)
+{
+
+}
+
+void Update_BanDai(float _Delta, GameEngineState* _State);
+void Update_BanDai(float _Delta, GameEngineState* _State);
+void End_BanDai(GameEngineState* _State);
+void End_FromSoft(GameEngineState* _State);
+
+//AnyButtonBack->Transform.SetLocalPosition(float4(0.0f, -92.5f));
+//ButtonFont[0]->On();
+//ButtonFont[1]->On();
+
+
+
+
+void TitleLogo::Ending()
+{
+	Logostate.ChangeState(eLogoState::EndLogo);
+
+}
+
+
+
+
+
+
+void TitleLogo::SkipButton(GameEngineState* _State, eLogoState _StateName)
+{
+	if(GameEngineInput::IsDown('E', this))
+	{
+		_State->ChangeState(_StateName);
+		return;
+	}
 }
--- DirectX_3DTeamPortfolio/GameEngineContents/TitleLogo.h
+++ DirectX_3DTeamPortfolio/GameEngineContents/TitleLogo.h
@@ -4,6 +4,30 @@
 // 설명 :
 class TitleLogo : public GameEngineActor
 {
+	enum class eLogoState
+	{
+		None,
+		BanDai,
+		FromSoft,
+		DarkSoul,
+		AnyButton,
+		EndLogo,
+		Done,
+	};
+
+	enum class eButtonState
+	{
+		None,
+		BStart,
+		BExit,
+	};
+
+	enum ButtonType
+	{
+		GameStart,
+		GameExit,
+	};
+
 public:
 	// constructer destructer
 	TitleLogo();
@@ -15,10 +39,33 @@ public:
 	TitleLogo& operator = (const TitleLogo& _Other) = delete;
 	TitleLogo& operator = (TitleLogo&& _Other) noexcept = delete;
 
+	void Ending();
+
 protected:
 	void Start() override;
 	void Update(float _Delta) override;
 	void Release() override;
+
+
+	// 로고 State
+	void Update_BanDai(float _Delta, GameEngineState* _State);
+	void Update_FromSoft(float _Delta, GameEngineState* _State);
+	void Update_DarkSoul(float _Delta, GameEngineState* _State);
+	void Update_AnyButton(float _Delta, GameEngineState* _State);
+
+	void End_BanDai(GameEngineState* _State);
+	void End_FromSoft(GameEngineState* _State);
+	void End_DarkSoul(GameEngineState* _State);
+	void End_AnyButton(GameEngineState* _State);
+
+	void Start_Done(GameEngineState* _State);
+
+	// 버튼 State
+	void Update_BanDai(float _Delta, GameEngineState* _State);
+	void Update_BanDai(float _Delta, GameEngineState* _State);
+
+	void End_BanDai(GameEngineState* _State);
+	void End_FromSoft(GameEngineState* _State);
 
 private:
 	std::shared_ptr<class GameEngineSpriteRenderer> DarkBack = nullptr;
@@ -28,14 +75,21 @@ private:
 	std::shared_ptr<class GameEngineSpriteRenderer> Inc_Logo = nullptr;
 
 	std::shared_ptr<class GameEngineSpriteRenderer> AnyButtonBack = nullptr;
-	std::shared_ptr<class GameEngineUIRenderer> FontRender;
+	std::shared_ptr<class GameEngineUIRenderer> AnyButtonFont;
+
+	void SkipButton(GameEngineState* _State, eLogoState _StateName); // E
+	void ButtonCreate();
+	std::vector<std::shared_ptr<class GameEngineUIRenderer>> ButtonFont;
+	std::shared_ptr<GameEngineUIRenderer> UtilRender;
+
 	float FontScale = 20.0f;
 	bool AnyBack = false;
 	bool AnyBackColor = false;
 	bool FontEnter = false;
 
-	float Time = 0.0f;
-	float FromSoftLogoTime = 4.0f;
+	GameEngineState Logostate;
+
+	GameEngineState ButtonState;
 
 };
 
